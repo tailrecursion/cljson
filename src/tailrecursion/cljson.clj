cb50b35fdeb7b4628b9da422465518a31583ad86
Micha Niskin <asdf@ubergibson.com>
2013-06-10 17:20:36 -0400

    multimethods; tighten up

--- src/tailrecursion/cljson.clj
+++ src/tailrecursion/cljson.clj
@@ -3,31 +3,23 @@
     [clojure.data.json  :as j]
     [clojure.walk       :as w]))
 
+(defmulti ->str type)
+(defmulti ->clj #(and (string? %) (case (first %) \ufdd0 0 \ufdd1 1 false)))
+
+(defmethod ->str :default             [x] (str x))
+(defmethod ->str clojure.lang.Keyword [x] (format "\ufdd0'%s" (name x)))
+(defmethod ->str clojure.lang.Symbol  [x] (format "\ufdd1'%s" (name x)))
+(defmethod ->clj false                [x] x)
+(defmethod ->clj 0                    [x] (keyword (subs x 2)))
+(defmethod ->clj 1                    [x] (symbol  (subs x 2)))
+
 (extend-type clojure.lang.Keyword
   j/JSONWriter
-  (-write [this out]
-    (#'j/write-string (format "\ufdd0'%s" (name this)) out)))
+  (-write [this out] (#'j/write-string (->str this) out)))
 
 (extend-type clojure.lang.Symbol
   j/JSONWriter
-  (-write [this out]
-    (#'j/write-string (format "\ufdd1'%s" (name this)) out)))
+  (-write [this out] (#'j/write-string (->str this) out)))
 
-(defn- write-key-fn [x]
-  (cond
-    (keyword? x) (format "\ufdd0'%s" (name x))
-    (symbol?  x) (format "\ufdd1'%s" (name x))
-    :else        (str x)))
-
-(defn- read-key-fn [x]
-  (let [kwsym? #(and (string? %) (< 2 (count %)))
-        kw     #(keyword (subs % 2))
-        sym    #(symbol  (subs % 2))
-        kwsym  #(case (first %) \ufdd0 (kw %) \ufdd1 (sym %) %)]
-    (if (kwsym? x) (kwsym x) x)))
-
-(defn write-str [x]
-  (j/write-str x :key-fn write-key-fn))
-
-(defn read-str [x]
-  (w/postwalk read-key-fn (j/read-str x :key-fn read-key-fn)))
+(defn write-str [x] (j/write-str x :key-fn ->str))
+(defn read-str  [x] (w/postwalk ->clj (j/read-str x :key-fn ->clj)))
