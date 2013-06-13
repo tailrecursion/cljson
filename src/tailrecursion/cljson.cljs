4278908b819913f882672532acd270c0bf687fa3
Micha Niskin <asdf@ubergibson.com>
2013-06-13 19:23:27 -0400

    convert to and from string in both clj and cljs

--- src/tailrecursion/cljson.cljs
+++ src/tailrecursion/cljson.cljs
@@ -1,13 +1,13 @@
 (ns tailrecursion.cljson)
 
-(defn clj->cljson [x]
+(defn encode [x]
   (let [mapa    #(apply array (map %1 %2))
         type-id #(cond (seq? %) 0 (vector? %) 1 (map? %) 2 (set? %) 3)]
     (cond (map?     x)  (array (type-id x) (mapa (partial map encode) x)) 
           (coll?    x)  (array (type-id x) (mapa encode x)) 
           :else         x)))
 
-(defn cljson->clj [x]
+(defn decode [x]
   (let [ctor #(nth [() [] {} #{}] (first %)) 
         a?   #(js* "~{} instanceof Array" %)
         m?   #(and (a? %) (= 2 (first %)))
@@ -15,3 +15,6 @@
     (cond (m? x)  (into {} (mapv (partial map decode) (second x)))
           (a? x)  (seq* (into (ctor x) (mapv decode (second x)))) 
           :else   x)))
+
+(defn clj->cljson [x] (.stringify js/JSON (encode x)))
+(defn cljson->clj [x] (decode (.parse js/JSON x)))
