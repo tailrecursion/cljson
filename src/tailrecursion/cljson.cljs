1f84b36b2a3906311f4e322fadec79902a99163b
Micha Niskin <asdf@ubergibson.com>
2013-06-13 19:25:40 -0400

    need mapv cause map entries must be vectors

--- src/tailrecursion/cljson.cljs
+++ src/tailrecursion/cljson.cljs
@@ -3,7 +3,7 @@
 (defn encode [x]
   (let [mapa    #(apply array (map %1 %2))
         type-id #(cond (seq? %) 0 (vector? %) 1 (map? %) 2 (set? %) 3)]
-    (cond (map?     x)  (array (type-id x) (mapa (partial map encode) x)) 
+    (cond (map?     x)  (array (type-id x) (mapa (partial mapv encode) x)) 
           (coll?    x)  (array (type-id x) (mapa encode x)) 
           :else         x)))
 
@@ -12,7 +12,7 @@
         a?   #(js* "~{} instanceof Array" %)
         m?   #(and (a? %) (= 2 (first %)))
         seq* #(if (list? %) (reverse %) %)]
-    (cond (m? x)  (into {} (mapv (partial map decode) (second x)))
+    (cond (m? x)  (into {} (mapv (partial mapv decode) (second x)))
           (a? x)  (seq* (into (ctor x) (mapv decode (second x)))) 
           :else   x)))
 
