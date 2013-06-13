a136f25dc46a3228d3a737618682b7c8348105db
Micha Niskin <asdf@ubergibson.com>
2013-06-13 17:50:42 -0400

    new approach

--- src/tailrecursion/cljson.clj
+++ src/tailrecursion/cljson.clj
@@ -1,25 +1,24 @@
 (ns tailrecursion.cljson
   (:require
-    [clojure.data.json  :as j]
-    [clojure.walk       :as w]))
+    [clojure.data.json  :as j]))
 
-(defmulti ->str type)
-(defmulti ->clj #(and (string? %) (case (first %) \ufdd0 0 \ufdd1 1 false)))
+(defn encode [x]
+  (let [type-id #(cond (seq? %) 0 (vector? %) 1 (map? %) 2 (set? %) 3)]
+    (cond (coll?    x)  [(type-id x) (mapv encode x)]
+          (keyword? x)  (format "\ufdd0'%s" (subs (str x) 1))
+          (symbol?  x)  (format "\ufdd1'%s" (str x))
+          :else         x)))
 
-(defmethod ->str :default             [x] (str x))
-(defmethod ->str clojure.lang.Keyword [x] (format "\ufdd0'%s" (name x)))
-(defmethod ->str clojure.lang.Symbol  [x] (format "\ufdd1'%s" (name x)))
-(defmethod ->clj false                [x] x)
-(defmethod ->clj 0                    [x] (keyword (subs x 2)))
-(defmethod ->clj 1                    [x] (symbol  (subs x 2)))
 
-(extend-type clojure.lang.Keyword
-  j/JSONWriter
-  (-write [this out] (#'j/write-string (->str this) out)))
+(defn decode [x]
+  (let [ctor #(nth [() [] {} #{}] (first %)) 
+        kw?  #(= \ufdd0 (first %))
+        sym? #(= \ufdd1 (first %))
+        seq* #(if (list? %) (reverse %) %)]
+    (cond (vector?  x)  (seq* (into (ctor x) (mapv decode (second x)))) 
+          (kw?      x)  (keyword (subs x 2))
+          (sym?     x)  (symbol (subs x 2))
+          :else         x)))
 
-(extend-type clojure.lang.Symbol
-  j/JSONWriter
-  (-write [this out] (#'j/write-string (->str this) out)))
-
-(defn write-str [x] (j/write-str x :key-fn ->str))
-(defn read-str  [x] (w/postwalk ->clj (j/read-str x :key-fn ->clj)))
+(defn clj->cljson [x] (j/write-str (encode x)))
+(defn cljson->clj [x] (decode (j/read-str x)))
