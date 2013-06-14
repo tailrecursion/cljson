a4d331785de692ed917c38c64e8c9c7527719b7c
Micha Niskin <asdf@ubergibson.com>
2013-06-13 20:53:40 -0400

    use objects for maps, sets, and lists

--- src/tailrecursion/cljson.clj
+++ src/tailrecursion/cljson.clj
@@ -3,24 +3,24 @@
     [clojure.data.json  :as j]))
 
 (defn encode [x]
-  (let [type-id #(cond (seq? %) 0 (vector? %) 1 (map? %) 2 (set? %) 3)]
-    (cond (map?     x)  [(type-id x) (mapv (partial mapv encode) x)]
-          (coll?    x)  [(type-id x) (mapv encode x)]
+  (let [type-id #(cond (seq? %) "l" (map? %) "m" (set? %) "s")]
+    (cond (vector?  x)  (mapv encode x)
+          (coll?    x)  {(type-id x) (mapv encode x)} 
           (keyword? x)  (format "\ufdd0'%s" (subs (str x) 1))
           (symbol?  x)  (format "\ufdd1'%s" (str x))
           :else         x)))
 
 (defn decode [x]
-  (let [ctor #(nth [() [] {} #{}] (first %)) 
-        m?   #(and (vector? %) (= 2 (first %)))
+  (let [ctor {"l" () "m" {} "s" #{}}
+        m?   #(and (map? %) (= "m" (first (first %))))
         kw?  #(and (string? %) (= \ufdd0 (first %))) 
         sym? #(and (string? %) (= \ufdd1 (first %))) 
         seq* #(if (list? %) (reverse %) %)]
-    (cond (m?       x)  (into {} (mapv (partial mapv decode) (second x)))
-          (vector?  x)  (seq* (into (ctor x) (mapv decode (second x)))) 
+    (cond (vector?  x)  (mapv decode x)
+          (map?     x)  (let [[k v] (first x)] (into (ctor k) (mapv decode v)))
           (kw?      x)  (keyword (subs x 2))
           (sym?     x)  (symbol (subs x 2))
           :else         x)))
 
-(defn clj->cljson [x] (j/write-str (encode x)))
+(defn clj->cljson [x] (j/write-str (encode x) :escape-unicode false :escape-slash false))
 (defn cljson->clj [x] (decode (j/read-str x)))
--- src/tailrecursion/cljson.cljs
+++ src/tailrecursion/cljson.cljs
@@ -2,19 +2,27 @@
 
 (defn encode [x]
   (let [mapa    #(apply array (map %1 %2))
-        type-id #(cond (seq? %) 0 (vector? %) 1 (map? %) 2 (set? %) 3)]
-    (cond (map?     x)  (array (type-id x) (mapa (partial mapv encode) x)) 
-          (coll?    x)  (array (type-id x) (mapa encode x)) 
-          :else         x)))
+        type-id #(cond (seq? %) "l" (map? %) "m" (set? %) "s")]
+    (cond (vector? x) (mapa encode x)
+          (coll?   x) (js-obj (type-id x) (mapa encode x)) 
+          :else       x)))
 
 (defn decode [x]
-  (let [ctor #(nth [() [] {} #{}] (first %)) 
-        a?   #(js* "~{} instanceof Array" %)
-        m?   #(and (a? %) (= 2 (first %)))
-        seq* #(if (list? %) (reverse %) %)]
-    (cond (m? x)  (into {} (mapv (partial mapv decode) (second x)))
-          (a? x)  (seq* (into (ctor x) (mapv decode (second x)))) 
-          :else   x)))
+  (let [ctor      {"l" () "m" {} "s" #{}}
+        a?        #(js* "~{} instanceof Array" %)
+        o?        #(js* "~{} instanceof Object" %)
+        l?        #(and (.hasOwnProperty % "l") "l")
+        m?        #(and (.hasOwnProperty % "m") "m")
+        s?        #(and (.hasOwnProperty % "s") "s")
+        kw?       #(and (string? %) (= \ufdd0 (first %))) 
+        sym?      #(and (string? %) (= \ufdd1 (first %))) 
+        seq*      #(if (list? %) (reverse %) %)
+        coll-typ  (and (o? x) (or (m? x) (l? x) (s? x)))]
+    (cond (a?   x)  (mapv decode x)
+          coll-typ  (into (ctor coll-typ) (mapv decode (aget x coll)))
+          (kw?  x)  (keyword (subs x 2))
+          (sym? x)  (symbol (subs x 2))
+          :else     x)))
 
 (defn clj->cljson [x] (.stringify js/JSON (encode x)))
 (defn cljson->clj [x] (decode (.parse js/JSON x)))
