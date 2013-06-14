c36a31d48dfae998dd826f66f6c458f552350f5c
Alan Dipert <alan@dipert.org>
2013-06-14 05:48:38 -0400

    tag literals
    - inst and uuid native
    - extension supported via *data-readers* and *default-data-reader-fn* on both sides

--- src/tailrecursion/cljson.clj
+++ src/tailrecursion/cljson.clj
@@ -2,7 +2,8 @@
   (:require
     [clojure.data.json :refer [write-str read-str]]))
 
-(defprotocol Collection
+(set! *warn-on-reflection* true)
+(defprotocol Tagged
   (tag [o]))
 
 (defprotocol Encode
@@ -15,17 +16,17 @@
        ~@(mapcat identity
            (for [[classes impls] (partition 2 (partition-by class? specs))
                  class classes]
-             (list* class impls))))))
+             `(~class ~@impls))))))
 
-(extends-protocol Collection
+(extends-protocol Tagged
   clojure.lang.PersistentArrayMap
   clojure.lang.PersistentHashMap
-  (tag [_] "m")
+  (tag [_] "cljson/map")
   clojure.lang.ISeq
   clojure.lang.PersistentList
-  (tag [_] "l")
+  (tag [_] "cljson/list")
   clojure.lang.PersistentHashSet
-  (tag [_] "s"))
+  (tag [_] "cljson/set"))
 
 (extends-protocol Encode
   clojure.lang.MapEntry
@@ -37,6 +38,10 @@
   clojure.lang.PersistentList
   clojure.lang.PersistentHashSet
   (encode [o] {(tag o) (mapv encode o)})
+  java.util.Date
+  (encode [o] {"inst" (.format (.get @#'clojure.instant/thread-local-utc-date-format) o)})
+  java.util.UUID
+  (encode [o] {"uuid" (str o)})
   clojure.lang.Keyword
   (encode [o] (format "\ufdd0'%s" (subs (str o) 1)))
   clojure.lang.Symbol
@@ -50,16 +55,23 @@
 
 (declare decode)
 
-(defmulti decode-coll (comp key first))
+(defmulti decode-tag (comp key first))
 
-(defmethod decode-coll "m" [m]
-  (into {} (map decode (get m "m"))))
+(defmethod decode-tag "cljson/map" [m]
+  (into {} (map decode (get m "cljson/map"))))
 
-(defmethod decode-coll "l" [m]
-  (apply list (map decode (get m "l"))))
+(defmethod decode-tag "cljson/list" [m]
+  (apply list (map decode (get m "cljson/list"))))
 
-(defmethod decode-coll "s" [m]
-  (set (map decode (get m "s"))))
+(defmethod decode-tag "cljson/set" [m]
+  (set (map decode (get m "cljson/set"))))
+
+(defmethod decode-tag :default [m]
+  (let [[tag val] (first m)]
+    (if-let [reader (or (get (merge default-data-readers *data-readers*) (symbol tag))
+                        *default-data-reader-fn*)]
+      (reader (decode val))
+      (throw (RuntimeException. (str "No reader function for tag " tag))))))
 
 (defmulti decode-str #(.charAt ^String % 0))
 
@@ -74,7 +86,7 @@
 (defn decode
   [v]
   (cond (vector? v) (mapv decode v)
-        (map? v)    (decode-coll v)
+        (map? v)    (decode-tag v)
         (string? v) (decode-str v)
         :else v))
 
--- src/tailrecursion/cljson.cljs
+++ src/tailrecursion/cljson.cljs
@@ -1,20 +1,22 @@
 (ns tailrecursion.cljson
-  (:require-macros [tailrecursion.cljson :refer [extends-protocol]]))
+  (:require-macros [tailrecursion.cljson :refer [extends-protocol]])
+  (:require [cljs.reader :as reader]
+            [goog.date.DateTime :as date]))
 
-(defprotocol Collection
+(defprotocol Tagged
   (tag [o]))
 
 (defprotocol Encode
   (encode [o]))
 
-(extends-protocol Collection
+(extends-protocol Tagged
   cljs.core.PersistentArrayMap
   cljs.core.PersistentHashMap
-  (tag [_] "m")
+  (tag [_] "cljson/map")
   cljs.core.ISeq
-  (tag [_] "l")
+  (tag [_] "cljson/list")
   cljs.core.PersistentHashSet
-  (tag [_] "s"))
+  (tag [_] "cljson/set"))
 
 (extends-protocol Encode
   cljs.core.Vector
@@ -23,8 +25,12 @@
   cljs.core.PersistentHashMap
   cljs.core.ISeq
   cljs.core.PersistentHashSet
+  (encode [o] (doto (js-obj) (aset (tag o) (into-array (map encode o)))))
+  js/Date
   (encode [o] (doto (js-obj)
-                (aset (tag o) (into-array (map encode o)))))
+                (aset "inst" (.toUTCIsoString (date/fromRfc822String (str o))))))
+  cljs.core/UUID
+  (encode [o] (doto (js-obj) (aset "uuid" (.-uuid o))))
   js/String, js/Boolean, js/Number, nil
   (encode [o] o)))
 
@@ -34,17 +40,27 @@
 
 (declare decode)
 
-(defmulti decode-coll
-  #(js* "(function(o){for(var k in o) return k;})(~{})" %))
+(defn get-tag [o]
+  (js* "(function(o){for(var k in o) return k;})(~{})" %))
 
-(defmethod decode-coll "m" [o]
-  (into {} (map decode (aget o "m"))))
+(defmulti decode-tag get-tag)
 
-(defmethod decode-coll "l" [o]
-  (apply list (map decode (aget o "l"))))
+(defmethod decode-tag "cljson/map" [o]
+  (into {} (map decode (aget o "cljson/map"))))
 
-(defmethod decode-coll "s" [o]
-  (set (map decode (aget o "s"))))
+(defmethod decode-tag "cljson/list" [o]
+  (apply list (map decode (aget o "cljson/list"))))
+
+(defmethod decode-tag "cljson/set" [o]
+  (set (map decode (aget o "cljson/set"))))
+
+(defmethod decode-tag :default [o]
+  (let [tag (get-tag o)
+        val (decode (aget o tag))]
+    (if-let [reader (or (get @reader/tag-table tag)
+                        @reader/*default-data-reader-fn*)]
+      (reader val)
+      (throw (js/Error. (str "No reader function for tag " tag))))))
 
 (defn array? [o]
   (js* "(~{} instanceof Array)" o))
@@ -55,7 +71,7 @@
 (defn decode
   [v]
   (cond (array? v)  (mapv decode v)
-        (object? v) (decode-coll v)
+        (object? v) (decode-tag v)
         :else v))
 
 (defn cljson->clj
