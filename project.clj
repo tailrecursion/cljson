ce16179ff141064de81dc38f1d2198a3ff15c271
Micha Niskin <asdf@ubergibson.com>
2013-06-17 16:01:01 -0400

    Update tests

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :url "https://github.com/tailrecursion/cljson"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure   "1.5.1"]
-                 [cheshire "5.2.0"]
-                 [org.clojure/data.generators "0.1.2"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.reader "0.7.4"]
+                 [org.clojure/data.generators "0.1.2"]
+                 [cheshire "5.2.0"]]
   :repl-options {:init-ns tailrecursion.cljson}
   :plugins [[lein-cljsbuild "0.3.2"]]
   :cljsbuild {:builds
